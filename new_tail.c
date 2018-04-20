844926c1df8da9c4188cf5ece6533b042e6d4ecf
Sherpa <sherpal@debian-9.3.0-template>
2018-04-20 15:41:58 -0700

    Added other implimentations(1arg and 3 arg) to new_tail.c

--- new_tail.c
+++ new_tail.c
@@ -3,17 +3,35 @@
 #include<stdlib.h>
 #include<string.h>
 //max no of chars
-//function to count number of lines in a textfile 
 const int MAX_CHAR =  256;
-int countLine(FILE * textfile, char content[], int size)
+//function to count number of lines in a textfile 
+int countLine(FILE * textfile, char * content, const int size)
 {
 int line = 0;
 while(!feof(textfile)){
-    fgets(content, size, textfile)
+    fgets(content, size, textfile);
     line++;
 }
+line--; //remove extra line read at end
 return line;
 }
+//funtion to print specific lines if given the startingLine
+void printSpecificLine(FILE * textfile, char * content, const int size, int currentLine, int startLine)
+{
+int i = currentLine;//counter
+while(!feof(textfile)){
+     if(i == startLine)
+     { 
+        while(!feof(textfile)){
+        fgets(content, size, textfile);//only print the last n lines
+        printf("%s" , content);
+        }
+        return;
+     }
+     fgets(content, size, textfile); //read one line as a string
+     i++;
+     }
+ }
 int main(int argc,char*argv[])
 {
 //if more than three argument was passed after ./new_tail
@@ -26,38 +44,64 @@ if(argc > 4)
 else if(argc == 2)
 {
      FILE * fpointer;
-     char content[256]; //for storing content
+     char content[MAX_CHAR]; //for storing content
      fpointer = fopen(argv[1],"r"); //open file passed
      if(fpointer == NULL)//couldn't open file
      {
       fprintf(stderr, "Cannot open file\n");
       exit(0);
      }
-     int totalLine = countLine(fpointer, content[], MAX_CHAR)
+     int totalLine = countLine(fpointer, content, MAX_CHAR);
+     printf("%d",totalLine);
      int startLine = totalLine - 5;
      int currentLine = 1;
-     while(!feof(fpointer)){
-     if(currentLine == startLine)
-     {
-        
-           
- 
-
-     }
-fgets(content, 256, fpointer); //read one line as a string
-        currentLine++;
-     }
+     printSpecificLine(fpointer, content, MAX_CHAR, currentLine, startLine);
      fclose(fpointer); //close file
 }
-else if(argc ==1) //no arguments after ./new_cat
+else if(argc == 3)
+{
+    if(argv[1]=="-n")
+    {
+     FILE * fpointer;
+     char content[MAX_CHAR]; //for storing content
+     fpointer = fopen(argv[3],"r"); //open file passed
+     if(fpointer == NULL)//couldn't open file
+     {
+      fprintf(stderr, "Cannot open file\n");
+      exit(0);
+     }
+     int totalLine = countLine(fpointer, content, MAX_CHAR);
+     int startLine = totalLine - atoi(argv[2]);
+     int currentLine = 1;
+     printSpecificLine(fpointer, content, MAX_CHAR, currentLine, startLine);
+     fclose(fpointer); //close file
+     }
+    else
+     {
+      FILE * fpointer;
+     char content[MAX_CHAR]; //for storing content
+     fpointer = fopen(argv[1],"r"); //open file passed
+     if(fpointer == NULL)//couldn't open file
+     {
+      fprintf(stderr, "Cannot open file\n");
+      exit(0);
+     }
+     int totalLine = countLine(fpointer, content, MAX_CHAR);
+     int startLine = totalLine - atoi(argv[3]);
+     int currentLine = 1;
+     printSpecificLine(fpointer, content, MAX_CHAR, currentLine, startLine);
+     fclose(fpointer); //close file
+    }
+}
+else if(argc == 1) //no arguments after ./new_cat
 {
     char content[256];
     printf("type in text to cat\n hit Ctrl+D when you are done\n");
-    while(fgets(content,256, stdin))
+    while(fgets(content,MAX_CHAR, stdin))
     {
       printf("%s",content); //print whatever was input
     }
 }
 return 0;
 }
- 
+
