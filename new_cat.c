852051c0e05a807dac2b0a3faa4fd753bcebd377
Sherpa <sherpal@debian-9.3.0-template>
2018-04-19 17:11:33 -0700

    Implmented more than one argument situation, i.e. an error

--- new_cat.c
+++ new_cat.c
@@ -4,7 +4,22 @@
 #include<string.h>
 main(int argc,char*argv[])
 {
-
+if(argc > 2)
+{
+     fprintf(stderr, "allowed syntax is: ./new_cat [file.txt]\nfile.txt is optional\nno more than 1 argument accepted");
+}
+else if(argc == 2)
+{
+     FILE * fpointer;
+     fpointer = fopen(argv[1],"r");
+     char content[256];
+     
+     while(!feof(fpointer)){
+ 	fget(content,256,fpointer);
+        puts(content);
+     }
+     close(fpointer);
+}
 
 
 
