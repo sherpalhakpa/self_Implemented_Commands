596946e64832b83aa49caed3bc8582ca7a8bbf95
Sherpa <sherpal@debian-9.3.0-template>
2018-04-19 19:05:29 -0700

    Added no argument implementation to new_cat

--- new_cat.c
+++ new_cat.c
@@ -4,29 +4,38 @@
 #include<string.h>
 int main(int argc,char*argv[])
 {
+//if more than one argument was passed after ./new_cat
 if(argc > 2)
 {
      fprintf(stderr, "allowed syntax is: ./new_cat [file.txt]\nfile.txt is optional\nno more than 1 argument accepted\n");
      exit(0);
 }
+//if one argument i.e . filename is passed after ./new_cat
 else if(argc == 2)
 {
      FILE * fpointer;
-     char content[256];
-     fpointer = fopen(argv[1],"r");
-     if(fpointer == NULL)
+     char content[256]; //for storing content
+     fpointer = fopen(argv[1],"r"); //open file passed
+     if(fpointer == NULL)//couldn't open file
      {
       fprintf(stderr, "Cannot open file\n");
       exit(0);
      }
      while(!feof(fpointer)){
-        fgets(content, 256, fpointer);
+        fgets(content, 256, fpointer); //read one line as a string
         printf("%s", content);
      }
-     fclose(fpointer);
+     fclose(fpointer); //close file
 }
-
-
+else if(argc ==1) //no arguments after ./new_cat
+{
+    char content[256];
+    printf("type in text to cat\n hit Ctrl+D when you are done\n");
+    while(fgets(content,256, stdin))
+    {
+      printf("%s",content); //print whatever was input
+    }
+}
 return 0;
 }
 
